6129103276fb2b93913a636630ebe738c54c2391
Andrew Katz <andrewkatz00@gmail.com>
2020-03-02 17:29:04 -0800

    Fix config path

--- spec/ynar/api_spec.rb
+++ spec/ynar/api_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Ynar::Api do
-  let(:config) { Ynar::Config.new('spec/support/config.yaml') }
+  let(:config) { Ynar::Config.new(File.expand_path('../support/config.yaml', __dir__)) }
 
   describe '.new' do
     subject { described_class.new(config) }
